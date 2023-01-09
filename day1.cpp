d40170feccfc0ff3209c7a1f009c2f03afc03efb
DatProJack <alex.tang.yang@gmail.com>
2023-01-08 20:56:23 -0600

    imported

--- day1.cpp
+++ day1.cpp
@@ -1,1 +1,27 @@
-test
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  ifstream file("data.txt");
+  int a,b,c,d;
+  int count=0;
+
+  file >> b >> c >> d;
+  while(file.eof()==false)
+  {
+    
+    a=b;
+    b=c;
+    c=d;
+    file>>d;
+    if(b+c+d>a+b+c)
+      count++;
+   
+    
+  }
+
+  cout<<count;
+}
